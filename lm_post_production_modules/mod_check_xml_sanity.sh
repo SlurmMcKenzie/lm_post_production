35eb174390877d25d12fb9c06a7f00419960df3f
Slurm McKenzie <kris@textmaschine.com>
2015-05-31 11:19:47 +0200

    change xml-sanitizer-module

--- lm_post_production_modules/mod_check_xml_sanity.sh
+++ lm_post_production_modules/mod_check_xml_sanity.sh
@@ -2,22 +2,24 @@
 # replace <C>-Tags in intro, tableheader and box header through ""
 
 echo "Repairing file for XML-Export"
+echo "<C> -> <code>"
 
 # TODO: Was ist, wenn es mehrere <C>-Tags gibt? Anpassen!
 
 arg1=$(grep -e '@V:.*<C>' -e '@KT:.*<C>' -e '@TT:.*<C>' -e '@ZT:.*<C>' article.txt)
 	if [ -z "${arg1}" ] ; then
-		echo "Nothing to repair."
+		echo "Keine deplazierten Code-Tags gefunden."
 	else
 		sed -i \
-		-e '/@V/s/<C>/"/' \
-		-e '/@V/s/<C>/"/' \
-		-e '/@TT/s/<C>/"/' \
-		-e '/@TT/s/<C>/"/' \
-		-e '/@KT/s/<C>/"' \
-		-e '/@KT/s/<C>/"/' \
-		-e '/@ZT/s/<C>/"/' \
-		-e '/@ZT/s/<C>/"/' article.txt
-		echo "Replace <C>-Tags through quotation marks."
+		-e '/@V/s/<C>/\"/g' \
+		-e '/@V/s/<C>/\"/g' \
+		-e '/@TT/s/<C>/\"/g' \
+		-e '/@TT/s/<C>/\"/g' \
+		-e '/@KT/s/<C>/\"/g' \
+		-e '/@KT/s/<C>/\"/g' \
+		-e '/@ZT/s/<C>/\"/g' \
+		-e '/@ZT/s/<C>/\"/g' article.txt
 	fi
 echo "Done."
+
+echo "Sorting picture tags in News"
